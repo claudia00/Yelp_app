1caae7c241fc45821efd30c5fb2cbf0251a6da11
kairi <kairi@kairis-MacBook-Pro.local>
2018-09-22 14:37:10 -0400

    Yelp project add to repository

---
+++ Yelp/BusinessesViewController.swift
@@ -0,0 +1,70 @@
+//
+//  BusinessesViewController.swift
+//  Yelp
+//
+//  Created by Timothy Lee on 4/23/15.
+//  Copyright (c) 2015 Timothy Lee. All rights reserved.
+//
+
+import UIKit
+
+class BusinessesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    var businesses: [Business]!
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        Business.searchWithTerm(term: "Thai", completion: { (businesses: [Business]?, error: Error?) -> Void in
+            
+                self.businesses = businesses
+                self.tableView.reloadData()
+                if let businesses = businesses {
+                    for business in businesses {
+                        print(business.name!)
+                        print(business.address!)
+                    }
+                }
+            
+            }
+        )
+        
+        /* Example of Yelp search with more search options specified
+         Business.searchWithTerm(term: "Restaurants", sort: .distance, categories: ["asianfusion", "burgers"]) { (businesses, error) in
+                self.businesses = businesses
+                 for business in self.businesses {
+                     print(business.name!)
+                     print(business.address!)
+                 }
+         }
+         */
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+        
+        
+        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+            if businesses != nil{
+                return businesses!.count
+            }else{
+                return 0
+            }
+        }
+        
+        func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath){
+            let cell = tableView.dequeueReusableCell(withIdentifier: "BusinessCell", for IndexPath: indexPath) as! BusinessCell
+            
+            cell.business = businesses[indexPath.row]
+       return cell
+        }
+    }
+
+}
